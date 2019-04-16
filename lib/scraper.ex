e35e14bff4a68b21ce8dd13f4ade1926135d2cd3
Riley Miller <rileymiller@mymail.mines.edu>
2019-04-15 22:53:07 -0600

    progress on scraper

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -11,7 +11,7 @@ defmodule Scraper do
                         locations = 
                             response.body
                             |> Floki.find(".sidearm-schedule-game-row")
-                            |> Enum.map(&extract_location_name_and_id/1)
+                            |> Enum.map(&extract_opponent_date_score/1)
                             |> Enum.sort(&(&1.name < &2.name))
 
                         {:ok, locations}   
@@ -21,5 +21,22 @@ defmodule Scraper do
             _ -> :error
         end
     end
+    
+    defp extract_opponent_date_score({_tag, children}) do
+      {_, _, [game_date]} =
+          Floki.raw_html(children)
+          |> Floki.find(".sidearm-schedule-game-opponent-date")
+          |> hd()
+
+      {_, _, [opponent_name]} =
+          Floki.raw_html(children)
+          |> Floki.find("sidearm-schedule-game-opponent-name a")
+          |> hd()
+
+      %{game_date: game_date, opponent_name: opponent_name}
+
+    end
+
+
 
 end
